e4180df5c2ed10cb583470c7f6e0e1aeaee005fa
Jessica Chou <j4chou@gmail.com>
2016-06-19 15:16:04 -0700

    sort Zeroes in place

---
+++ moveZero.js
@@ -0,0 +1,22 @@
+/*
+Given an array of numbers, write a function to move all 0's to the end of it 
+while maintaining the relative order of the non-zero elements.
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, 
+nums should be [1, 3, 12, 0, 0]. You must do this in-place w/o making a copy of the array.
+*/
+function sortZeroes(arr) {
+  var end = arr.length-1;
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = i; j < arr.length - 1; j++) {
+      if (arr[j] === 0) {
+        while (j !== arr.length - 1) {
+          var temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          j++;
+        }
+      }
+    }
+  }
+return arr;
+}
