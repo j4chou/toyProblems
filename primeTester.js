c58a97ba1e9ed03064875c81382a117f87766fa1
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:20:24 -0700

    primeTester

--- primeTester.js
+++ primeTester.js
@@ -1,5 +1,6 @@
 function primeTester(n) {
   if (n === 1) { return false; }
+  // If n does not divide equally until up to its square root, it's a prime
   for (var i = 2; i <= Math.sqrt(n); i++) { 
     if (n % i === 0) {
       return false;
