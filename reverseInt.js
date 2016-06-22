14c9eec57419fe7753cee6a31b0b569d834ce0ab
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:25:59 -0700

    reverseInt

--- reverseInt.js
+++ reverseInt.js
@@ -1,7 +1,11 @@
 function reverseInt(n) {
   var reverse = 0;
+
+  // Until n is not zero
   while (n !== 0) {
+    // Keep multiplying by ten and add remainder of number % 10
     reverse = (reverse*10) + (n%10);
+    // Parse int to convert n into a whole number
     n = parseInt(n/10);
   }
   return reverse;
