afd0acdf0a16c36631033a09bde360b279a3017a
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:33:49 -0700

    swapVowels

--- swapVowels.js
+++ swapVowels.js
@@ -5,11 +5,12 @@ var j = string.length - 1;
 var arr = string.split("");
 
 while (i < j) {
+  // Check beginning characters to see if it's a vowel
   while (i < j && vowels.indexOf(arr[i]) === -1) {
     i++;
     continue;
   }
-
+  // Check characters from end to see if it's a vowel
   while (j > i && vowels.indexOf(arr[j]) === -1) {
     j--;
     continue;
