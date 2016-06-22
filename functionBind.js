4d4337823c010ed6e8d18dc067062ea782c177f0
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:46:21 -0700

    FunctionBind

--- functionBind.js
+++ functionBind.js
@@ -1,18 +1,27 @@
 var bind = function(func, context) {
+  // Get additional arguments passed in
   var args = Array.prototype.slice.call(arguments,2);
   return function() {
+    // Also get argument passed into func
     var args2 = Array.prototype.slice.call(arguments);
+    // Concatenate arguments
     var args3 = args.concat(args2);
+    // Apply context and arguments to function
     return func.apply(context, args3);
   }
 };
 
 Function.prototype.bind = function(context) {
+  // Function is the object 'bind' is called on
   var fn = this;
+  // Capture additional arguments
   var args = Array.prototype.slice.call(arguments,1);
   return function() {
+    // Capture arguments passed into function
     var args2 = Array.prototype.slice.call(arguments);
+    // Concatenate arguments
     var args3 = args.concat(args2);
+    // Apply context and arguments to function
     return fn.apply(context, args3);
   }
 };
