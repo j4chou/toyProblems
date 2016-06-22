2676c8a4d62d0784b5e6580739350a1c2b9e8a94
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:29:43 -0700

    Stack

--- Stack.js
+++ Stack.js
@@ -3,11 +3,14 @@ var Stack = function() {
   this.counter = 0;
 
   this.push = function(val) {
+    // Use counter as index to store valu
     this.storage[this.counter] = val;
+    // Increase counter after storing
     this.counter++;
   }
 
   this.pop = function() {
+    // Decrease counter before deleting to get to the value
     if (this.counter > 0) {
       this.counter--;
     }
