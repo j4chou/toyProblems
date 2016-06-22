67c6e77b39db33e24864fc9c082d8c9164447524
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:28:35 -0700

    convertFraction

--- convertFraction.js
+++ convertFraction.js
@@ -1,9 +1,9 @@
 function fractionConvert(num) {
   var count = 1;
-
+  // Until the decimal leaves no remainder, keep increasing count
   while ( parseFloat(num * count).toFixed(6) % 1 !== 0) {
     count++;
   }
-
+  // Multiple top and bottom by count to reach simplified fraction
   return parseInt(num*count) + '/' + count;
 }
