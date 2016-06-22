78596733786fad5bf879deb04cfe2bb700615b1c
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:20:10 -0700

    Balanced Parens

--- balancedParens.js
+++ balancedParens.js
@@ -1,8 +1,10 @@
 function balancedParens(string) {
   var count = 0;
+  // Iterate through string
   for (var i = 0; i < string.length; i++) {
     if (string[i] === "(") {
       count++;
+      // If count is greater than zero, there has been an opening parenthesis
     } else if (string[i] === ")" && count > 0) {
       count--;
     }
