7c31d08093f08bdc1d942472838bd0263ea8a2b8
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:09:13 -0700

    longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,8 +1,9 @@
 function longestPalindrome(string) {
   var longest = "";
-
+// Use nested for loops to capture every possible string combination
   for (var i = 0; i < string.length; i++) {
     for (var j = i; j < string.length; j++) {
+      // Check every substring
       var currString = string.substring(i, j+1);
       if (isPalindrome(currString)) {
         if (currString.length > longest.length) {
@@ -22,8 +23,9 @@ function isPalindrome(string) {
   } else {
     half = Math.floor(string.length / 2) - 1;
   }
-
+// Check if the string is a palindrome, only up until half the char length
   for (var i = 0; i < half; i++) {
+    // If first char isn't same as last, going inwards, not a palindrome
     if (string[i] !== string[string.length - 1 - i]) {
       return false;
     }
