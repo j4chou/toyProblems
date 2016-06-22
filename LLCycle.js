c77f0ee0b5186e373a986c520546443ec86152b9
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:03:56 -0700

    LL Cycle

--- LLCycle.js
+++ LLCycle.js
@@ -1,8 +1,9 @@
 var hasCycle = function(linkedList) {
   var fast = linkedList;
   var slow = linkedList;
-
+  // Use two different pointers
   while(fast.next.next !== null && fast.next !== null) {
+    // If the values are the same at one point, there's a cycle
     if (fast.next.next === slow.next) {
       return true;
     }
