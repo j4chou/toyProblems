3cfd13701ca51c0403f214370a0f05ceab25fcf3
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:14:18 -0700

    addDigits

--- addDigits.js
+++ addDigits.js
@@ -3,11 +3,12 @@ var sum;
 
   function recurse(total) {
     sum = 0;
+    // Convert total to string in order to iterate through
     total = total.toString();
     for (var i = 0; i < total.length; i++) {
       sum += total[i];
     }
-
+    // If the sum is not a single digit, keep adding its numbers
     if (sum.toString().length > 1) {
       recurse(sum);
     }
