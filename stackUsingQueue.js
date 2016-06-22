4ef90bc529a51911e93ae016fb44e5a4ebff877c
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:32:34 -0700

    Stack w/ Queue

--- stackUsingQueue.js
+++ stackUsingQueue.js
@@ -3,16 +3,19 @@ var Queue = function() {
   var outbox = new Stack();
 
   this.enqueue = function(val) {
+    // Add value to inbox
     inbox.push(val);
   };
 
   this.dequeue = function() {
-
+   // If there is nothing in outbox
     if (outbox.size() === 0) {
+      // Push inbox's last values into outbox
       while (inbox.size() > 0) {
         outbox.push(inbox.pop());
       }
     }
+    // Remove outbox's last value (which is first value in inbox)
     return outbox.pop();
   };
 
