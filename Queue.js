2a10375a1fc1322f06775cb0c55029ef9fe7bf9e
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:23:16 -0700

    Queue

--- Queue.js
+++ Queue.js
@@ -3,12 +3,16 @@ var Queue = function() {
   this.tail = 0;
   this.storage = {};
 
+  // Queue is first in, first out. 
+
   this.enqueue = function(val) {
+    // Add value to end of queue
     this.storage[this.tail] = val;
     this.tail++;
   }
 
   this.dequeue = function() {
+    // Delete value at head of queue
     if (this.tail >= this.head) {
       var temp = this.storage[this.head];
       delete this.storage[this.head];
