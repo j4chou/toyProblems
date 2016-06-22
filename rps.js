04c6ae81ead8e9af253a7e201cd9a5b0e1e6486c
Jessica Chou <j4chou@gmail.com>
2016-06-21 19:28:04 -0700

    RockPaperScissors

--- rps.js
+++ rps.js
@@ -3,11 +3,15 @@ function rps(rounds) {
   var combos = ["r"."p","s"];
 
   function recurse(str) {
+    // If string's length is equal to round number
     if (str.length === rounds) {
+      // Store string in result array
       results.push(str);
       return;
     }
+    // Iterate thru possible combinations
     for (var i = 0; i < combos.length; i++) {
+      // Store combination and recurse 
       recurse(str + combos[i]);
     }
   }
