74c6034494b63b4f8a2a6dafc78486b8059f7568
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:16:46 -0700

    allAnagrams

--- anagrams.js
+++ anagrams.js
@@ -2,13 +2,16 @@ function allAnagrams(string) {
   var results = {};
 
   function recurse(input, str) {
+    // Store string as key in an object
     if (input.length === string.length) {
       results[input] = input;
     }
+    // Iterate through string to get all possible anagrams from each letter
     for (var i = 0; i < str.length; i++) {
       recurse(input + str[i], str.slice(0,i) + str.slice(i+1)) 
     }
   }
  recurse("",string)
+ // Since results were stored in an object, the keys will be duplicate-free
  return Object.keys(results);
 }
