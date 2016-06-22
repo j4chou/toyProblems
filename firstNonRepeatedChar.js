1d00421d564b72684812091c8a900bcea353941d
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:41:23 -0700

    First Non-Repeated Char

--- firstNonRepeatedChar.js
+++ firstNonRepeatedChar.js
@@ -1,8 +1,10 @@
 function firstNonRepeatedChar(string) {
   var obj = {};
+  // Store character as key in object, and quantity as value
   for (var i = 0; i < string.length; i++) {
     obj[string[i]] = obj[string[i]] + 1 || 1;
   }
+  // Iterate over string to get the first character (source of truth)
   for (var i = 0; i < string.length; i++) {
     if (obj[string[i]] === 1) {
       return string[i];
