b6ab6bf7c210e7958cb8234ad699e5fc7e920d67
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:22:23 -0700

    BubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,9 @@
 function bubbleSort(array) {
+  // Use double for-loop to compare all numbers at each index
   for (var i = 0; i < array.length; i++) {
     for (var j = 0; j < array.length; j++) {
       if (array[j] > array[j+1]) {
+        // Swap variables
         var temp = array[j+1];
         array[j+1] = array[j];
         array[j] = temp;
