7106638e8e868889bee698f4681537857bde2167
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:56:59 -0700

    isSubsetOf

--- isSubsetOf.js
+++ isSubsetOf.js
@@ -1,6 +1,8 @@
 Array.prototype.isSubsetOf = function(input) {
   var result = true;
+  // Iterate throough collection
   for (var i = 0; i < this.length; i++) {
+    // Check if item in collection is also found in input
     if (input.indexOf(this[i]) === -1) {
       result = false;
     }
