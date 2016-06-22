68c29534a67496bbb7472714075ed0629e00d5c2
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:21:15 -0700

    BinarySearchArr

--- bsa.js
+++ bsa.js
@@ -6,8 +6,10 @@ function binarySearch(array, target) {
   while (min <= max) {
     if (array[mid] === target) {
       return mid;
+      // If target is in the first half, change max bounds to half
     } else if (target < array[mid]) {
       max = mid - 1;
+      // If target is in upper half, change min bounds to half
     } else if (target > array[mid]) {
       min = mid + 1;
     }
