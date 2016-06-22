df2d2fd5e0d4ac5012533f50b077c8bb064436f9
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:29:26 -0700

    eventLibrary

--- eventLibrary.js
+++ eventLibrary.js
@@ -1,15 +1,17 @@
 var mixEvents = function(obj) {
   var events = {};
 
-  // trigger can take additional arguments and pass to the listeners
+  // Trigger can take additional arguments and pass to the listeners
   obj.trigger = function(event) {
     var args = Array.prototype.slice.call(arguments, 1);
     for (var i = 0; i < events[event].length; i++) {
+      // Search for event in events library and apply arguments
       events[event][i].apply(null, args);
     }
   };
 
   obj.on = function(event, callback) {
+    // Store event callback in events object
     events[event] = events[event] || [];
     events[event].push(callback);
   };
