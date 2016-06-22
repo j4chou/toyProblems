a83f7611e1573300258e151da54e45c2f93a0d5f
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:26:35 -0700

    composePipe

--- composePipe.js
+++ composePipe.js
@@ -1,6 +1,8 @@
 var compose = function() {
   var fn = Array.prototype.slice.call(arguments);
+  // Compose returns a function that may take in an argument
   return function(arg) {
+    // Call argument on list of functions from right to left
     return fn.reduceRight(function(prev,next) {
       return next(prev)
     }, arg);
