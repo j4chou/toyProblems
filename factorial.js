c66bb1490e73f234fbc373f597d561dc60961a6c
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:32:19 -0700

    Factorial

--- factorial.js
+++ factorial.js
@@ -1,8 +1,11 @@
 function factorial(num) {
   var product = 1;
+
   if (num === 0) {
+    // There's still one way to produce that number
     return product;
   } else {
+    // Multiply result of product by the following integers until you've reached the number
     for (var i = 1; i <= num; i++) {
       product *= i;
     }
