7314a4ba2a6ce2eaec41671c84e4c33d202dd2be
Jessica Chou <j4chou@gmail.com>
2016-06-21 18:54:01 -0700

    insertionSort

--- insertionSort.js
+++ insertionSort.js
@@ -2,12 +2,13 @@ function insertionSort(array) {
   for (var i = 1; i < array.length; i++) {
     var temp = array[i];
     var j = i - 1;
-
+    // While the value is less than the value before it
     while (array[i] < array[j]) {
+      // Keep swapping
       var temp = array[i];
       array[i] = array[j];
       array[j] = temp;
-
+      // Decrease index value until the value finds the correct spot
       if (j > 0) {
         j--;
       }
