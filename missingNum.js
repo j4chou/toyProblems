93a4021d0c5429b5d9b189ed40b9f115ba4a6012
Jessica Chou <j4chou@gmail.com>
2016-07-07 21:53:20 -0700

    Fix typo

--- missingNum.js
+++ missingNum.js
@@ -1,12 +1,12 @@
 const missingNum = (arr) => {
   let n = arr.length + 1;
 
-  // formual for linear series of n numbers
+  // Formula for linear series of n numbers
   let expectedSum = n * (n+1) / 2;
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];  
   }
-  // subtract sum from expected to find the missing number
+  // Subtract sum from expected to find the missing number
   return expectedSum - sum;
 }
